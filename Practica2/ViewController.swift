db456c9c6d8d55ac34dd4eb137578e243a75a1ec
analourdescr <ana_lourdes98@hotmail.com>
2021-08-25 17:23:14 -0700

    Primercomit

--- Practica2/ViewController.swift
+++ Practica2/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var txtEdad: UITextField!
+    @IBOutlet weak var lblResultado: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblNombre.text = "Andres Reyes"
     }
 
-
+    @IBAction func doTapDecirEdad(_ sender: Any) {
+        lblResultado.text = "\(lblNombre.text!) tiene \(txtEdad.text!) años"
+    }
+    
 }
 
+//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
